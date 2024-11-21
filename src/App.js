67e3ae432740040ae90381c59fac56b56171d9c1
Rishabh Yadav <87073393+rishabh777ry@users.noreply.github.com>
2024-11-21 17:56:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
     const handleSubmit = async () => {
         try {
             const parsedInput = JSON.parse(jsonInput);
-            const { data } = await axios.post("https://api-check-coral.vercel.app/", parsedInput);
+            const { data } = await axios.post("https://api-check-two.vercel.app/api/index", parsedInput);
             setResponse(data);
         } catch (err) {
             alert("Invalid JSON or error in API call");
